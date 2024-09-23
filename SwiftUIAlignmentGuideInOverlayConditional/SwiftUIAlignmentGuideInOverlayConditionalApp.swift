58a843b712e2352843afc77c5890aa24b7e9396b
Simon Nickel <simonnickel@googlemail.com>
2024-09-23 11:32:26 +0200

    Initial Commit

---
+++ SwiftUIAlignmentGuideInOverlayConditional/SwiftUIAlignmentGuideInOverlayConditionalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAlignmentGuideInOverlayConditionalApp.swift
+//  SwiftUIAlignmentGuideInOverlayConditional
+//
+//  Created by Simon Nickel on 23.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAlignmentGuideInOverlayConditionalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
